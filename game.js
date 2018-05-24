5bfdaa133860ba975c1f376b594850fda1a216f8
iziken <iziken@inbox.ru>
2018-05-24 18:08:28 +0700

    github

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
 'use strict';
+
+class Vector {
+  constructor(x=0, y=0) {
+    this.x = x;
+    this.y = y;
+  }
+
+
+  plus(plusVector) {
+    if (!(plusVector)) {
+      throw new Error('Передан не вектор, сложение не выполнено.');
+    }
+    return new Vector(this.x + plusVector.x, this.y + plusVector.y);
+  }
+
+  times(multiplierVector = 1) {
+    return new Vector(this.x * multiplierVector, this.y * multiplierVector);
+  }
+}
+
+const start = new Vector(30, 50);
+const moveTo = new Vector(5, 10);
+const finish = start.plus(moveTo.times(2));
+
+console.log(`Исходное расположение: ${start.x}:${start.y}`);
+console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
