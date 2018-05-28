74f308ebe66ff0e035b7c445e54fbfb24f9984ba
iziken <iziken@inbox.ru>
2018-05-28 16:53:50 +0700

    github

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 class Vector {
   constructor(x=0, y=0) {
     this.x = x;
@@ -59,5 +61,126 @@ class Actor {
     }
     return this.left < moveActor.right && this.top < moveActor.bottom &&
             this.right > moveActor.left && this.bottom > moveActor.top;
+  }
+}
+
+class Level {
+  constructor(grid = [], actors = []) {
+    this.actors = actors.slice();
+    this.status = null;
+    this.finishDelay = 1;
+    this.grid = grid.slice();
+    this.height = this.grid.length;
+    this.width = Math.max(0, ...this.grid.map(element=> element.length));
+    this.player = this.actors.find(actor => actor.type === 'player');
+  }
+
+  isFinished() {
+    return this.status !== null && this.finishDelay < 0;
+  }
+
+  actorAt(moveActor) {
+    if (!(moveActor instanceof Actor)) {
+      throw new Error("Level: actorAt's argument is wrong");
+    }
+    return this.actors.find(actor => actor.isIntersect(moveActor));
+  }
+
+  obstacleAt(position, size) {
+    if (!((position instanceof Vector)&&(size instanceof Vector))) {
+      throw new Error("Передан не вектор.");
+    }
+
+    const topBorder = Math.floor(position.y);
+    const bottomBorder = Math.ceil(position.y + size.y);
+    const leftBorder = Math.floor(position.x);
+    const rightBorder = Math.ceil(position.x + size.x);
+
+    if (leftBorder < 0 || rightBorder > this.width || topBorder < 0) {
+      return 'wall';
+    }
+    if (bottomBorder > this.height) {
+      return 'lava';
+    }
+
+    for (let y = topBorder; y < bottomBorder; y++) {
+      for (let x = leftBorder; x < rightBorder; x++) {
+        const cell = this.grid[y][x];
+        if (cell) {
+          return cell;
+        }
+      }
+    }
+  }
+
+  removeActor(actor) {
+    const findInd = this.actors.indexOf(actor);
+    if (findInd !== -1) {
+      this.actors.splice(findInd, 1)
+    }
+  }
+
+  noMoreActors(typeActor) {
+    return !this.actors.some(actor => actor.type === typeActor);
+  }
+
+  playerTouched(touched, actor) {
+    if (this.status !== null) {
+      return
+    }
+    if (['lava', 'fireball'].some(element => element === touched )) {
+      this.status = 'lost';
+    }
+    if (touched === 'coin' && actor.type === 'coin') {
+      this.removeActor(actor);
+      if (this.noMoreActors('coin')) {
+        this.status = 'won';
+      }
+    }
+  }
+}
+
+
+const obstaclesDict = {
+  'x': 'wall',
+  '!': 'lava'
+};
+
+class LevelParser {
+  constructor(charsDict = {}) {
+    this.actorsLibrary = Object.assign({}, charsDict);
+  }
+
+  actorFromSymbol(char) {
+    return this.actorsLibrary[char];
+  }
+
+  obstacleFromSymbol(char) {
+    return obstaclesDict[char];
+  }
+
+  createGrid(arrayGrid = []) {
+    return arrayGrid.map(line => line.split('').map(char => this.obstacleFromSymbol(char)));
+  }
+
+  createActors(arrayActors = []) {
+    const actors = [];
+    arrayActors.forEach((itemY, y) => {
+      itemY.split('').forEach((itemX, x) => {
+        const constructorActors = this.actorFromSymbol(itemX);
+        if (typeof constructorActors !== 'function') {
+          return;
+        }
+        const result = new constructorActors(new Vector(x, y));
+        if (result instanceof Actor) {
+          actors.push(result);
+        }
+      });
+    });
+    return actors;
+  }
+
+  parse(plan) {
+    return new Level(this.createGrid(plan), this.createActors(plan));
   }
 }
